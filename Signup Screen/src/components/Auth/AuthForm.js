9b487791bd4023fb57310b65be8536225f8b1655
HRSumanth <130749610+HRSumanth@users.noreply.github.com>
2023-06-20 21:33:43 +0530

    Add files via upload

---
+++ Signup Screen/src/components/Auth/AuthForm.js
@@ -0,0 +1,116 @@
+import { useState, useRef, useContext } from 'react';
+import AuthContext from '../../Store/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  let content;
+
+  const authCtx = useContext(AuthContext);
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [error, setError] = useState();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if (enteredEmail && enteredPassword) {
+      setIsFormValid(true);
+    } else {
+      setError('All fields are required');
+      return;
+    }
+
+    setIsLoading(true);
+    let url;
+    if (isLogin && isFormValid) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBS8czfYR6fVHclpZF1rBED6NjtXlKSz2I';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBS8czfYR6fVHclpZF1rBED6NjtXlKSz2I';
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        authCtx.login(data.idToken);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+    <section className="auth bg-dark text-light p-3 w-25">
+      {error && <p style={{ color: 'red', textAlign: 'start' }}>*{error}</p>}
+      <h1 className="text-center">{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <label htmlFor="email" className="text-light">
+            Email
+          </label>
+          <input type="email" id="email" className="form-control col-xs-4" required ref={emailInputRef} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="text-light">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-control mb-2"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className="d-flex flex-column align-items-center">
+          {!isLoading && (
+            <button className="btn btn-primary">{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type="button"
+            className="btn btn-link toggle"
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Have an account? Login'}
+          </button>
+        </div>
+      </form>
+    </section>
+    </div>
+  );
+};
+
+export default AuthForm;
